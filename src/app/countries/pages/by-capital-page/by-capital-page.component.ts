4583e0edf226a8f64e2f18450d895184aa25d140
jjreina <jreinapascual@gmail.com>
2024-05-29 17:51:19 +0200

    Update search box component to emit search value

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-by-capital-page',
   templateUrl: './by-capital-page.component.html',
   styles: ``,
 })
-export class ByCapitalPageComponent {}
+export class ByCapitalPageComponent {
+  searchByCapital = (term: string): void => {
+    console.log('From capital page');
+    console.log(term);
+  };
+}
