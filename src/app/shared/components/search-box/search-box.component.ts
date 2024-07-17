53859e157095c6518630b41b8160e8d303477080
jjreina <jreinapascual@gmail.com>
2024-07-17 10:50:02 +0200

    refactor: Update search box component to debounce search input

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,13 +1,21 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Subject, debounceTime } from 'rxjs';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: ``,
 })
-export class SearchBoxComponent implements OnInit {
+export class SearchBoxComponent implements OnInit, OnDestroy {
   private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSubcription?: Subscription;
 
   @Input()
   public placeholder: string = '';
@@ -19,9 +27,15 @@ export class SearchBoxComponent implements OnInit {
   public onDebounce: EventEmitter<string> = new EventEmitter<string>();
 
   ngOnInit(): void {
-    this.debouncer.pipe(debounceTime(500)).subscribe((value) => {
-      this.onDebounce.emit(value);
-    });
+    this.debouncerSubcription = this.debouncer
+      .pipe(debounceTime(500))
+      .subscribe((value) => {
+        this.onDebounce.emit(value);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.debouncerSubcription?.unsubscribe();
   }
 
   public emitValue = (txtInput: string): void => {
